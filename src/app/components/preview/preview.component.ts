9dfd402fb647c2db7681df239be0b9622459f500
skhacker105 <skhacker105@gmail.com>
2022-11-24 15:14:54 +0530

    loaded info in preview component

--- src/app/components/preview/preview.component.ts
+++ src/app/components/preview/preview.component.ts
@@ -1,19 +1,48 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IUser } from 'src/app/models/user';
+import { Subject, takeUntil } from 'rxjs';
+import { IPersonal } from 'src/app/models/personal';
+import { BuilderService } from 'src/app/services/builder.service';
 
 @Component({
   selector: 'app-preview',
   templateUrl: './preview.component.html',
   styleUrls: ['./preview.component.scss']
 })
-export class PreviewComponent implements OnInit {
+export class PreviewComponent implements OnInit, OnDestroy {
 
-  user: IUser | undefined;
+  userId: string | undefined;
+  info: IPersonal | undefined;
+  isActive = new Subject();
 
-  constructor(private route: ActivatedRoute) { }
+  constructor(private route: ActivatedRoute, private builderService: BuilderService) { }
 
   ngOnInit(): void {
+    this.route.params
+    .pipe(takeUntil(this.isActive))
+    .subscribe(params => {
+      this.userId = params['id'];
+      this.loadUserInfo();
+    });
+  }
+
+  loadUserInfo() {
+    if (!this.userId) return;
+    this.builderService.getInfo(this.userId)
+    .pipe(takeUntil(this.isActive))
+    .subscribe({
+      next: (info) => {
+        if (info && info.length) this.info = info[0];
+      },
+      error: (err) => {
+        console.log('info load error = ', err);
+      }
+    });
+  }
+  
+  ngOnDestroy(): void {
+    this.isActive.next(false);
+    this.isActive.complete();
   }
 
 }
